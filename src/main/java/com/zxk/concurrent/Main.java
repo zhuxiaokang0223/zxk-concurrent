88af343f3e9d68f451ba0fda86e3401c2611be57
zhuxiaokang <xiaokang.zhu@pactera.com>
2018-03-23 15:54:39 +0800

    示例

--- src/main/java/com/zxk/concurrent/Main.java
+++ src/main/java/com/zxk/concurrent/Main.java
@@ -1,19 +1,9 @@
 package com.zxk.concurrent;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 /**
  * Created by zhuxiaokang on 2018/3/20.
  */
 
 public class Main {
-    static Logger logger = LogManager.getLogger(Main.class);
-
-
-    public static void main(String[] args){
-
-
-
-    }
+ 
 }
